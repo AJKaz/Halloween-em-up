e4a284c8ba2b7d9e1c85289628a1f88fa7857536
Jml6049 <jml6049@g.rit.edu>
2024-10-27 12:16:45 -0400

    Zombie attacks full implment

--- GameJam2024/Assets/Scripts/States/EnemyStates/EnemyAttackState.cs
+++ GameJam2024/Assets/Scripts/States/EnemyStates/EnemyAttackState.cs
@@ -13,14 +13,6 @@ public class EnemyAttackState : EnemyState
         
         bAttacking = true;
 
-        // TEMP: ZOMBIE doesn't have attack anim (ATM), so need this
-        // TODO: Once attack anim is added, can remove this and 
-        // bind attack damage via animation frame event, like witch has
-        if (enemy.enemyType == EnemyType.Zombie) {
-            enemy.TryDamagePlayer();
-            return;
-        }
-
         if (animator) {
             animator.SetTrigger("Attack1");
         }
